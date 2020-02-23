fd6163f1213fcfd188a8b86f06adec900b381097
Kiran Kumar <Kiran@Kirans-MacBook-Pro-2.local>
2020-02-23 16:07:13 +0000

    Adding my first file helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
 print("hello world")
+my_list =[1,2,3]
+print(my_list)
+
+print (type(my_list))
+
+my_tuple = (4,5,6)
+
+print(my_tuple)
+
+print (type(my_tuple))
+
+my_dict={1:"kiran", 2:"laks"}
+print(my_dict)
+"""dhh
+adjadf
+akdjalkd """
+
+print (type(my_dict))
