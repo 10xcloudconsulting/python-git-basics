fef2f2d60a4d1579fb8cb0efef9770180f53b7e9
Lakshmana Kattula <vivalaks@gmail.com>
2020-02-25 18:18:38 +0000

    Worked on lots of concepts in this file - Taking inputs (interative, env variables, command line args). Working with Files, etc.

--- basics/python_basics_104_func_circles.py
+++ basics/python_basics_104_func_circles.py
@@ -30,3 +30,97 @@ circle_dict = {1:0, 2:0, 3:0, 4:0, 5:0}
 print("Size of the circle dictionary is: {}".format(len(circle_dict)))
 for key in circle_dict:
     print("Key is: {}, Value is: {}".format(key, circle_dict[key]))
+
+# 4 Interactive session with a User to calculate and return area of circles
+
+print("Welcome to the Interactive Session to get the Area of a Circle")
+
+cont = "y"
+while(cont.lower() == "y"):
+    radius_interact = input("Please enter the radius of the circle to get the area: ")
+    area_interact = area_circle(int(radius_interact))
+    print("The area is : {:.2f}".format(area_interact))
+    cont = input("Do you want to do one more calculation [y to continue]: ")
+
+print("Thank you. See you again!")
+
+# 5 Working with environment variables
+import os
+print("HOME: " + os.environ.get("HOME", ""))
+
+# 6 Working with command line arguments
+import sys
+print("Command Line arguments are")
+print(sys.argv) #Returns a list
+
+# 7 Working with files
+file = open("input_radius_file.txt")
+print("The radius is: " + file.readline())
+print("The radius is: " + file.readline())
+print("The radii are: " + file.read())
+file.close()
+
+with open("input_radius_file.txt") as file:
+    print("The radii in the file are: " + file.read())
+
+# Iterate through the file (file object which is a sequence)
+with open("input_radius_file.txt") as file:
+    print(type(file))
+    for radius_line in file:
+        print(radius_line)
+
+# Keep the lines in the file in a list and can be iterated separately as needed. Use readlines for only small files. All file content is loaded to memory!
+with open("input_radius_file.txt") as file:
+    lines = file.readlines()
+    print(type(lines))
+    lines.sort()
+    print(lines)
+    for line in lines:
+        print(line)
+
+# Use strip to remove the newline character
+with open("input_radius_file.txt") as file:
+    for radius_line in file:
+        print(radius_line.strip())
+
+# Writing into a file. File open modes and arguments (w, a, r+, r and other modes).  w creates a file if not existing; overides if one exists.
+with open("output_circle_area.txt", "w") as out_file:
+    out_file.write("Radius: 4" + "Area: " + str(area_circle(4)))
+
+
+# 8 Working with Dictionaries. Use os module
+import os
+print("The current working directory is : " + os.getcwd())
+print("List of directories and files in CWD: ")
+print(os.listdir(os.getcwd()))
+dir = os.getcwd()
+for name in os.listdir(dir):
+    fullname = os.path.join(dir, name)
+    if os.path.isdir(fullname):
+        print("{} is a directory".format(fullname))
+    else:
+        print("{} is a file".format(fullname))
+
+# 9 Working with CSV files. Use Radius and Area as Headers.
+import csv
+
+circle_list = [[1,"area1"], [2,"area2"]]
+with open("./data/circle_rad_area.txt", "w") as file:
+    writer = csv.writer(file)
+    writer.writerows(circle_list)
+
+with open("./data/circle_rad_area.txt", "r") as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        print(row)
+        radius, area = row
+        print("Radius: {} Area: {}".format(radius, area))
+
+circles_list = [{"radius":10,"area":"area1"}, {"radius":20,"area":"area2"}, {"radius":30,"area":"area3"}]
+keys = ["radius", "area"]
+with open("./data/circle_rad_area.txt", "w") as file:
+    writer = csv.DictWriter(file, fieldnames=keys)
+    writer.writeheader()
+    writer.writerows(circles_list)
+
+# 10 Working with env variable INPUT_FILE getting radii and calculating areas and publishing all back into file into file given in env variable OUTPUT_FILE
