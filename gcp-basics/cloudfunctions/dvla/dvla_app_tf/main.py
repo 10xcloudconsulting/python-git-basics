b08d254683f60ef2450616023dc28e2ebd9aa3cc
Lakshmana Kattula <vivalaks@gmail.com>
2020-04-11 10:28:17 +0100

    First commit of DVLA Verify Vehicle Reg Cloud Func App

---
+++ gcp-basics/cloudfunctions/dvla/dvla_app_tf/main.py
@@ -0,0 +1,61 @@
+# Copyright 2018, Google, LLC.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START functions_ocr_setup]
+import base64
+import json
+import os
+
+from google.cloud import pubsub_v1
+from google.cloud import storage
+from google.cloud import translate
+from google.cloud import vision
+from flask import escape
+
+vision_client = vision.ImageAnnotatorClient()
+translate_client = translate.Client()
+publisher = pubsub_v1.PublisherClient()
+storage_client = storage.Client()
+
+project_id = os.environ['GCP_PROJECT']
+print("Project Id is : {}".format(project_id))
+
+with open('config.json') as f:
+    data = f.read()
+config = json.loads(data)
+# [END functions_ocr_setup]
+
+# [START functions_dvla_verify_vehicle_reg]
+def verify_vehicle_reg(request):
+    print('Entered verify_vehicle_reg function')
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
+
+# [END functions_dvla_verify_vehicle_reg]
